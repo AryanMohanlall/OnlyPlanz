cde729b3caa1f4f091a5e43c1c191289185f1872
AryanMohanlall <u23565536@tuks.co.za>
2026-02-04 16:44:20 +0200

    for fun

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const setTimeout = require('node:timers/promises').setTimeout;
 const app = express()
 const port = 3000
 
@@ -13,6 +14,17 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+async function spinner(){
+  const spinnerChars = ['|', '/', '-', '\\'];
+  while(true){
+    for( let char of spinnerChars){
+      process.stdout.write(`\r${char}`);
+      await setTimeout(100);
+    }
+  }
+}
+
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
+  spinner()
 });
