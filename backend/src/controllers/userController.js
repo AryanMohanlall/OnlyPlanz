8d4fc7009caf40cd75bd3b3e4d1d3abb3b7ed8d0
AryanMohanlall <u23565536@tuks.co.za>
2026-02-04 16:19:58 +0200

    tidy

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,23 +1,26 @@
 const userModel = require('../models/user');
 
-module.exports = {
-  getAllusers: (req, res) => {
+  const getAllusers = (req, res) => {
     const users = userModel.findAll();
-    // In a full MVC app, this would render a view (e.g., using Pug)
-    // res.render('user-list', { users }); 
     res.status(200).json(users);
-  },
+  }
 
-  getuserById: (req, res) => {
+  const getuserById = (req, res) => {
     const user = userModel.findById(req.params.id);
     if (!user) {
       return res.status(404).json({ error: 'user not found' });
     }
     res.status(200).json(user);
-  },
+  }
 
-  createuser: (req, res) => {
+  const createuser = (req, res) => {
     const newuser = userModel.create(req.body);
     res.status(201).json(newuser);
   }
+
+
+module.exports = {
+  getAllusers,
+  getuserById,
+  createuser
 };
