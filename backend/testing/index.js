5bf95401484a53c4af770d4fd504c8447c9d1766
0ptim0s <aryanmohanlall@gmail.com>
2026-02-15 20:43:48 +0200

    idk what i was doing

--- backend/testing/index.js
+++ backend/testing/index.js
@@ -1,1 +1,26 @@
 const socket = io("http://localhost:3000");
+
+
+const addRecieverMessage = (message)=>{
+    const log = document.querySelector('.message-log');
+    const newMessage = document.createElement('div');
+    newMessage.style.backgroundColor = "grey";
+    newMessage.style.width = "100%";
+    newMessage.style.height = "auto";
+    newMessage.style.color = "white";
+    newMessage.innerText = message;
+    log.appendChild(newMessage);
+}
+
+const addSenderMessage = (message)=>{
+    const log = document.querySelector('.message-log');
+    const newMessage = document.createElement('div');
+    newMessage.style.backgroundColor = "rgb(202, 221, 250)";
+    newMessage.style.width = "100%";
+    newMessage.style.height = "auto";
+    newMessage.innerText = message;
+    log.appendChild(newMessage);
+}
+
+addRecieverMessage("you")
+addSenderMessage("them");
